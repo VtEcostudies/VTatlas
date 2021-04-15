2f6be9e01b1861fdac31b0756b1c78cc13d70b56
HallworthMT <mhallworth@vtecostudies.org>
2021-04-15 15:45:38 -0400

    added ability to filter the fields returned

--- R/get_data.R
+++ R/get_data.R
@@ -6,13 +6,18 @@
 #' @param query \code{list} of query terms \code{list('order:Odonata','etc')}
 #' @param type options include "TotalObs" and 'occurrences'. TotalObs returns only the number of observations. No data.
 #' occurrence returns data associated with occurrence records. default = occurrences.
-#' @param fields defaults to 'all'
+#' @param fields defaults to 'all'. see \code{dataFields} for returnable fields
 #' @param quiet defaults to \code{FALSE}, if \code{TRUE} prints brief description to console
 #' @param pageSize defaults to NULL which returns all records
 #' @param spatialWKT well-known text for a \code{POLYGON} or \code{MULTIPOLYGON}, defaults to null
 #' @return \code{data frame} of results from query
 #' @seealso \code{dataFields} for description of fields and which fields can be queried
+#' @example
+#' \dontrun{queryAPI(query = list("order:Odonata"),
+#'                   type = 'occurrences',
+#'                   fields = c('taxon_name','longitude','latitude','year'))}
 #' @export
+#'
 queryAPI <- function(query,
                      type = 'occurrences',
                      fields = "all",
@@ -41,6 +46,9 @@ queryAPI <- function(query,
   # quick query to get the pageSize and report inital stats
   intialQuery <- suppressWarnings(jsonlite::fromJSON(readLines(queryURL)))
 
+  if(!fields=="all"){
+  queryURL <- paste0(queryURL,"&fl=",paste0(fields,collapse=","))
+  }
   # if pageSize is null get the total number of records
   ps <- ifelse(is.null(pageSize),intialQuery$totalRecords, pageSize)
 
